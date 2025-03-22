2dd3c774de1fe5c71347485829a91f9eda93a74e
Asmitumrao <ashmitumrao@gmail.com>
2025-03-22 11:55:48 +0530

    deployment fixes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const { configDotenv } = require('dotenv')
+const dotenv = require('dotenv').config()
 const express = require('express')
 const app = express()
-port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
